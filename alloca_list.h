efc28daa48197405855b3dbfa4acd8019d066027
David Bien <davidbien@hotmail.com>
2020-04-28 14:33:59 -0600

    alloca_list: work in progress

---
+++ alloca_list.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// alloca_list.h
+// 22FEB2020 David Bien
+// Implement a generic list using _alloca().
+
+template < class t_tyT >
+class AllocaList
+{
+    typedef AllocaList _tyThis;
+public:
+    typedef t_tyT _tyT;
+
+    
+
+}
