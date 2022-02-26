c3534c4c8eba6949a206d92c83454a011b99384e
David Bien <davidbien@hotmail.com>
2022-02-25 19:38:08 -0700

    object optimization header

--- obj_io.h
+++ obj_io.h
@@ -6,7 +6,8 @@
 // 24FEB2022
 
 #include "bienutil.h"
-#include "tiny_obj_loader.h"
+#include <limits>
+#include <unordered_map>
 
 __BIENUTIL_BEGIN_NAMESPACE
 
@@ -25,9 +26,70 @@ concept CxVertexHasFileWrite = requires( t_TyVertex _v, t_TyFile _f )
 template < typename t_TyVertex >
 concept CxVertexHasMemIO = requires( t_TyVertex _v, void * _pv, size_t _stLength )
 {
-  { t_TyVertex::GetMaxWriteLength(void) } -> size_t;
-  { _v.ReadMem( _pv, _stLength ) } -> size_t;
-  { _v.WriteMem( ( _pv ) ) } -> size_t;
+  { t_TyVertex::GetMaxWriteLength() } -> std::convertible_to<size_t>;
+  { _v.ReadMem( _pv, _stLength ) } -> std::convertible_to<size_t>;
+  { _v.WriteMem( ( _pv ) ) } -> std::convertible_to<size_t>;
+};
+
+struct _OO_MappedWriteableFile
+{
+  typedef _OO_MappedWriteableFile _TyThis;
+public:
+  FileObj m_foFile;
+  FileMappingObj m_fmoFile;
+  const size_t m_kstMaxVertexWriteSize;
+  static const size_t s_kstVertexGrowBy = 8192;
+  size_t m_stCurrentMappedSize;
+  uint8_t * m_pbyCur{ nullptr };
+  uint8_t * m_pbyEnd{ nullptr };
+  string m_strFileOut;
+
+  _OO_MappedWriteableFile( const char * _pcFileOut, size_t _kstMaxVertexWriteSize ) noexcept( false )
+    : m_strFileOut( _pcFileOut ),
+      m_foFile( CreateReadWriteFile( _pcFileOut ) ),
+      m_kstMaxVertexWriteSize( _kstMaxVertexWriteSize ),
+      m_stCurrentMappedSize( _kstMaxVertexWriteSize * s_kstVertexGrowBy )
+  {
+    VerifyThrowSz( m_foFile.FIsOpen(), "Unable to open [%s] for reading and writing.", &m_strFileOut[0] );
+    int iResult = FileSetSize( m_foFile.HFileGet(), m_stCurrentMappedSize ); // Set initial size.
+    VerifyThrowSz( !iResult, "Failed to set file size on [%s], errno[%d].", &m_strFileOut[0], GetLastErrNo() );
+    m_fmoFile.SetHMMFile( MapReadWriteHandle( m_foFile.HFileGet() ) );
+    VerifyThrowSz( m_fmoFile.FIsOpen(), "Mapping failed for [%s].", &m_strFileOut[0] );
+    m_pbyCur = (uint8_t*)m_fmoFile.Pv();
+    m_pbyEnd = m_pbyCur + m_stCurrentMappedSize;
+  }
+  template < class t_TyVertex >
+  void WriteVertex( t_TyVertex const & _rvtx )
+  {
+    if ( size_t( m_pbyEnd - m_pbyCur ) < m_kstMaxVertexWriteSize )
+      _SetMappedSize( m_stCurrentMappedSize + ( m_kstMaxVertexWriteSize * s_kstVertexGrowBy ) );
+    m_pbyCur += _rvtx.WriteMem( m_pbyCur );
+  }
+  // Reset the file to the final needed size and then write the indices.
+  template < class t_TyIndex >
+  void WriteIndices( const t_TyIndex * _pidxBegin, const t_TyIndex * _pidxEnd )
+  {
+    size_t stRemaining = ( m_pbyEnd - m_pbyCur );
+    size_t stNeeded = ( _pidxEnd - _pidxBegin ) * sizeof (*_pidxEnd);
+    if ( stNeeded != stRemaining )
+      _SetMappedSize( m_stCurrentMappedSize + ( stNeeded - stRemaining ) );
+    memcpy( m_pbyCur, _pidxBegin, stNeeded );
+    m_pbyCur = m_pbyEnd;
+  }
+private:
+  // Grow or shrink the mapped file.
+  void _SetMappedSize( const size_t _kstNewSize )
+  {
+    m_stCurrentMappedSize = _kstNewSize;
+    uint8_t * pbyOldMapping = (uint8_t*)m_fmoFile.Pv();
+    (void)m_fmoFile.Close();
+    int iFileSetSize = FileSetSize( m_foFile.HFileGet(), m_stCurrentMappedSize );
+    VerifyThrowSz( !iFileSetSize, "FileSetSize() failed for file [%s].", &m_strFileOut[0] );
+    m_fmoFile.SetHMMFile( MapReadWriteHandle( m_foFile.HFileGet() ) );
+    VerifyThrowSz( m_fmoFile.FIsOpen(), "Remapping the failed for file [%s].", &m_strFileOut[0] );
+    m_pbyEnd = (uint8_t*)m_fmoFile.Pv() + m_stCurrentMappedSize;
+    m_pbyCur = (uint8_t*)m_fmoFile.Pv() + ( m_pbyCur - pbyOldMapping );
+  }
 };
 
 // ObjOptimizer:
@@ -39,30 +101,17 @@ class ObjOptimizer
 public:
   typedef t_TyVertex _TyVertex;
 
-  
-
   // OptimizeTinyObjShapes:
   // Read the vertices from all shapes in [_iterShapesBegin, _iterShapesEnd) in index-order and de-duplicate vertices.
   // Write the optimized vertices and indices to _pcFileOut 
   template < class t_TyIterShapes >
-  void OptimizeTinyObjShapes( tinyobj::attrib_t _toAttribs, t_TyIterShapes _iterShapesBegin, t_TyIterShapes _iterShapesEnd, const char * _pcFileOut )
+  void OptimizeTinyObjShapes( tinyobj::attrib_t const & _toAttribs, t_TyIterShapes _iterShapesBegin, t_TyIterShapes _iterShapesEnd, const char * _pcFileOut )
   {
-    const size_t kstMaxVertexWriteSize = _TyVertex::GetMaxWriteLength(); // Ensure that our writeable mapped file always has at least this much space at the end.
-    const size_t kstGrowBySizeBytes = kstMaxVertexWriteSize * 4096; // grow the file by this each time - we will truncate it at the end.
-
-    // Open _pcFileOut as a mapped writeable file.
-    FileObj foFile( CreateReadWriteFile( _pcFileOut ) );
-    VerifyThrowSz( foFile.FIsOpen(), "Unable to open [%s] for reading and writing.", _pcFileOut );
-    int iResult = FileSetSize( foFile.HFileGet(), kstGrowBySizeBytes ); // Set initial size.
-    VerifyThrowSz( !iResult, "Failed to set file size on [%s], errno[%d].", _pcFileOut, GetLastErrNo() );
-    FileMappingObj fmoFile( MapReadWriteHandle( foFile.HFileGet() ) );
-    VerifyThrowSz( fmoFile.FIsOpen(), "Mapping failed for [%s].", _pcFileOut );
-    size_t stCurrentMappedSize = kstGrowBySizeBytes;
-    uint8_t * pbyCur = (uint8_t*)fmoFile.Pv();
-    uint8_t * pbyEnd = pbyCur + stCurrentMappedSize;
+    _OO_MappedWriteableFile mwf( _pcFileOut, _TyVertex::GetMaxWriteLength() );
 
     // First move through and count the vertices so that we can reserve space in the index array to avoid reallocation during loading. (speed)
     size_t nIndices = 0;
+    t_TyIterShapes iterShapesCur = _iterShapesBegin;
     for ( ; _iterShapesEnd != iterShapesCur; ++iterShapesCur )
       nIndices += iterShapesCur->mesh.indices.size();
     VerifyThrowSz( nIndices < (numeric_limits< uint32_t >::max)(), "nIndices is larger than UINT32_MAX." );
@@ -71,58 +120,56 @@ public:
     rgIndices.reserve( nIndices );
 
     // Make space for the count of unique vertices, and the count of indices - we can write the count of indices right now:
-    pbyCur += 2 * sizeof( uint32_t ); // Also reserve a spot for the offset of the start of the indices.
-    *(uint32_t*)pbyCur = nIndices;
-    pbyCur += sizeof( uint32_t );
+    mwf.m_pbyCur += 2 * sizeof( uint32_t ); // Also reserve a spot for the offset of the start of the indices.
+    *(uint32_t*)mwf.m_pbyCur = (uint32_t)nIndices;
+    mwf.m_pbyCur += sizeof( uint32_t );
+    const size_t stAlignOfVertex = alignof( _TyVertex );
+    static_assert( stAlignOfVertex >= alignof( uint32_t ) );
+    // Align to the vertex alignment:
+    mwf.m_pbyCur += stAlignOfVertex - ( mwf.m_pbyCur - (uint8_t*)mwf.m_fmoFile.Pv() ) % stAlignOfVertex;
 
     typedef unordered_map< _TyVertex, uint32_t > _TyMapUniqueVert;
     _TyMapUniqueVert mapUniqueVertices;
 
     uint32_t nUniqueVertCur = 0; // As we write unique vertices to the file we increment this global index of vertices.
-    t_TyIterShapes iterShapesCur = _iterShapesBegin;    
+    iterShapesCur = _iterShapesBegin;    
     for ( ; _iterShapesEnd != iterShapesCur; ++iterShapesCur )
     {
-      for ( const auto& idxShapeCur : iterShapesCur->mesh.indices )
+      for ( const tinyobj::index_t & ridxShapeCur : iterShapesCur->mesh.indices )
       {
         const tinyobj::shape_t & rspCur = *iterShapesCur;
-        pair< _TyMapUniqueVert, bool > pib = mapUniqueVertices.emplace( piecewise_construct, forward_as_tuple( _toAttribs, idxShapeCur ), forward_as_tuple( nUniqueVertCur ) );
-        if ( pid.second ) // If unique vertex.
+        pair< _TyMapUniqueVert::const_iterator, bool > pib = mapUniqueVertices.emplace( piecewise_construct, forward_as_tuple( _toAttribs, ridxShapeCur ), forward_as_tuple( nUniqueVertCur ) );
+        if ( pib.second ) // If unique vertex.
         {
-          rgIndices.push_back( nUniqueVertCur++ )
+          rgIndices.push_back( nUniqueVertCur++ );
           // Finish import of vertex here - this saves time in the case where we have a duplicate vertex.
-          const_cast< _TyVertex >( pib.first->first ).FinishImport( _toAttribs, idxShapeCur );
+          const_cast< _TyVertex & >( pib.first->first ).FinishImport( _toAttribs, ridxShapeCur );
           // Write the vertex to the file.
-          if ( pbyEnd - pbyCur < kstMaxVertexWriteSize )
-          {
-            // Then we have to grow the mapped writeable file:
-            stCurrentMappedSize += kstGrowBySizeBytes;
-            void * pvOldMapping = fmoFile.Pv();
-            (void)fmoFile.Close();
-            int iFileSetSize = FileSetSize( foFile.HFileGet(), stCurrentMappedSize );
-            VerifyThrowSz( !iFileSetSize, "FileSetSize() failed for file [%s].", _pcFileOut );
-            fmoFile.SetHMMFile( MapReadWriteHandle( m_foFile.HFileGet() ) );
-            VerifyThrowSz( m_fmoFile.FIsOpen(), "Remapping the failed for file [%s].", _pcFileOut );
-            pbyMappedEnd = (uint8_t*)fmoFile.Pv() + stCurrentMappedSize;
-            pbyCur = (uint8_t*)fmoFile.Pv() + ( pbyCur - pvOldMapping );
-          }
-          pbyCur += pib.first->first->WriteMem( pbyCur );
+          mwf.WriteVertex( pib.first->first );
+          // We should always be aligned to a vertex after a write.
+          VerifyThrowSz( !( ( mwf.m_pbyCur - (uint8_t*)mwf.m_fmoFile.Pv() ) % stAlignOfVertex ), 
+            "Not aligned to a vertex after write. nUniqueVertCur[%u].", nUniqueVertCur );
         }
         else
-          rgIndices.push_back( pib->second ); // existing vertex - just record the index.
+        {
+#ifndef NDEBUG
+          // All elements of the vertex should match.
+          _TyVertex dbg_vtx( _toAttribs, ridxShapeCur );
+          dbg_vtx.FinishImport( _toAttribs, ridxShapeCur );
+          Assert( dbg_vtx.FCompare( pib.first->first, false ) );
+#endif //!NDEBUG
+          rgIndices.push_back( pib.second ); // existing vertex - just record the index.
+        }
+      }
     }
+    // Now write the count of vertices to the begining of the file:
+    ((uint32_t*)mwf.m_fmoFile.Pv())[0] = nUniqueVertCur;
+    // Write the offset of the start of the indices, this allows variable length per-vertex data.
+    ((uint32_t*)mwf.m_fmoFile.Pv())[1] = uint32_t( mwf.m_pbyCur - (uint8_t*)mwf.m_fmoFile.Pv() );
+    Assert( rgIndices.size() == nIndices );
+    mwf.WriteIndices( &rgIndices[0], &rgIndices[0] + nIndices );
   }
-  // Now write the count of vertices to the begining of the file:
-  *(uint32_t*)fmoFile.Pv() = nUniqueVertCur;
-  // Write the offset of the start of the indices, this allows variable length per-vertex data.
-  ((uint32_t*)fmoFile.Pv())[1] = uint32_t( pbyCur - (uint8_t*)fmoFile.Pv() );
-  Assert( rgIndices.size() == nIndices );
-  
-  // Now make sure we have enough room for the indices:
-  size_t nbyIndices = rgIndices.size() * sizeof( uint32_t );
-    if ( pbyEnd - pbyCur < kstMaxVertexWriteSize )
-    {
-    }
-  }
+
 
 };
 
