bb2e65e147640a13c24ff597b59d510f05854e50
David Bien <davidbien@hotmail.com>
2024-03-30 01:54:06 -0700

    locking ptr - specialize for boost upgradable locks

--- _lockingptr.h
+++ _lockingptr.h
@@ -7,6 +7,7 @@
 // Adapted to allow for multiple mutex classes and multiple locks, etc.
 
 #include "bienutil.h"
+#include <boost/thread/locks.hpp>
 
 __BIENUTIL_BEGIN_NAMESPACE
 
@@ -27,8 +28,7 @@ public:
   //  keyword to allow volatility to vary as "mutable" allows const to vary.
   LockingPtr( volatile t_Ty & _obj, volatile t_TyMutex & _mtx )
     : m_p( const_cast< t_Ty * >( &_obj ) )
-    , m_pMtx( const_cast< t_TyMutex * >( &_mtx ) )
-    , m_lock( *m_pMtx )
+    , m_lock( const_cast< t_TyMutex & >( _mtx ) )
   {
   }
   ~LockingPtr() 
@@ -38,13 +38,48 @@ public:
   LockingPtr & operator=( LockingPtr && ) = default;
 
   // Pointer behavior
-  t_Ty & operator*() { return *m_p; }
-  t_Ty * operator->() { return m_p; }
+  t_Ty & operator*() const { return *m_p; }
+  t_Ty * operator->() const { return m_p; }
 
 private:
   t_Ty * m_p;
-  t_TyMutex * m_pMtx;
   t_TyLockClass m_lock;
 };
+
+// Partially specialize for boost::upgrade_to_unique_lock<> to implement correctly.
+template < typename t_Ty, typename t_TyMutex > class LockingPtr< t_Ty, t_TyMutex, boost::upgrade_to_unique_lock< t_TyMutex > >
+{
+  typedef LockingPtr _TyThis;
+public:
+  typedef t_Ty _Ty;
+  typedef t_TyMutex _TyMutex;
+  typedef boost::upgrade_to_unique_lock< t_TyMutex > _TyLockClass;
+  typedef LockingPtr< const t_Ty, t_TyMutex, boost::upgrade_lock< t_TyMutex > > _TyUpgradeLockingPtr;
+
+  LockingPtr() = delete;
+  LockingPtr( const LockingPtr & ) = delete;
+  LockingPtr & operator=( const LockingPtr & ) = delete;
+
+  // We take a locking ptr to upgrade since we need to get the object pointer from it.
+  LockingPtr( _TyUpgradeLockingPtr const & _krlkUpgrade )
+    : m_p( const_cast< t_Ty * >( &*_krlkUpgrade ) )
+    , m_lock( _krlkUpgrade )
+  {
+  }
+  ~LockingPtr() 
+  { 
+  }
+  LockingPtr( LockingPtr && ) = default;
+  LockingPtr & operator=( LockingPtr && ) = default;
+
+  // Pointer behavior
+  t_Ty & operator*() const { return *m_p; }
+  t_Ty * operator->() const { return m_p; }
+
+private:
+  t_Ty * m_p;
+  t_TyLockClass m_lock;
+};
+
 
 __BIENUTIL_END_NAMESPACE
